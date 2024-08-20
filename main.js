ed92291e4c9555489b588b98d2867a5a83e7d278
Stephen Wright <wrighs6@wrighs6.com>
2024-08-20 16:19:52 -0400

    rainbow

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas");
-const wgl = canvas.getContext("webgl2");
+const gl = canvas.getContext("webgl2");
 
 function resizeCanvas() {
   let width = window.innerWidth;
@@ -8,8 +8,21 @@ function resizeCanvas() {
   canvas.width = width;
   canvas.height = height;
 
-  wgl.viewport(0, 0, width, height);
+  gl.viewport(0, 0, width, height);
 }
 
 resizeCanvas();
 window.addEventListener("resize" , resizeCanvas);
+
+function render(time) {
+  const r = time % 499.0 / 499.0;
+  const g = time % 569.0 / 569.0;
+  const b = time % 691.0 / 691.0;
+
+  gl.clearColor(r, g, b, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
